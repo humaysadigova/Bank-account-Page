e03d7c6bb45e3e8a4f2dabc4f9c6cf6261238c83
humaysadigova <h.sadiqova.hs@gmail.com>
2023-09-19 17:59:14 +0400

    update

--- main.js
+++ main.js
@@ -10,56 +10,70 @@ cardTitle = document.querySelector('#cardTitle'),
 amountInput = document.querySelector('#amountInput'),
 addBtn = document.querySelector('#addBtn'),
 withdrawBtn = document.querySelector('#withdrawBtn');
- function BankTransactions(name, password) {
-    this.name = name;
-    this.password = password;
+
+ function BankTransactions(BankName) {
+    this.bankName = BankName
+    this.name = null;
+    this.password = null;
     this.balance = 0;
 
     this.entry = function () {
         entre.classList.add('d-none');
         account.classList.remove('d-none');
-        account.classList.add('d-block');
     };
 
     this.addBalance = function (value) {
-        if (value = '') {
+        if (value == '') {
             alert("Please enter the amount!")
-        } else if (value <= 0) {
+        } else if ((value < 0)) {
             alert('Please enter a positive number!')
-        }else {this.balance += parseFloat(value);
+        }
+        else {this.balance += value;
             currentBalance.innerHTML = `Your balance: ${this.balance} `;
         }
         
     };
 
     this.withdraw = function (wdvalue){
-        if (wdvalue = '' || wdvalue < '0' || wdvalue > this.balance) {
+        if ((wdvalue == '') || (wdvalue < 0) || (wdvalue > this.balance)) {
             alert('Please, enter correct amount!')
         } else {
-            this.balance -= parseFloat(wdvalue);
-            currentBalance.innerHTML = `Your balance: ${this.balance} `;
+           this.balance -= wdvalue;
+           currentBalance.innerHTML = `Your balance: ${this.balance} `;
         }
     };
 
-    this.accountfunc = function () {
-        account.innerHTML = `Dear ${this.name}, welcome your bank account!`
+    this.accountfunc = function (user, password) {
+        this.name = user;
+        this.password = password;
+        cardTitle.innerHTML = `Dear ${this.name}, welcome your bank account!`
     }
-
 };
+
+const myBank = new BankTransactions('MyBank');
 
 signInBtn.addEventListener('click', function(){
     const username = nameInput.value;
     const userpassword = passwordInput.value;
-    const myBank = new BankTransactions(username, userpassword);
-    myBank.accountfunc();
+    myBank
+    myBank.accountfunc(username, userpassword);
     myBank.entry();
 });
 
 addBtn.addEventListener('click', function(){
-    const value = amountInput.value;
+    const value = +(amountInput.value);
     myBank.addBalance(value);
     amountInput.value = '';
-    
-})
+});
 
+withdrawBtn.addEventListener('click', function(){
+    const value = +(amountInput.value);
+    myBank.withdraw(value);
+    amountInput.value = '';
+});
 
+checkbox.addEventListener("change", function () {
+    passwordInput.type = checkbox.checked ? "text" : "password";
+});
+
+console.log(myBank);
